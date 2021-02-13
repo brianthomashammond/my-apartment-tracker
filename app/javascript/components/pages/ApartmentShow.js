7a29baa3ab3841403af4bed0c1d4e6a7a16f4c35
brianthomashammond <brianthomashammond@gmail.com>
2021-02-12 19:12:46 -0800

    add background and index card to apartment show, adjust button padding

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -8,24 +8,28 @@ export default class ApartmentShow extends Component {
         const { apartment, current_user, logged_in } = this.props
         return (
             <>
-                <div className="show-body" style={{ backgroundImage: `url(${image})` }}>
-                    <h3>{apartment.street}</h3>
-                    <p>{apartment.city}, {apartment.state}</p>
-                    <p>Manager Name: {apartment.manager}</p>
-                    <p>Manager Email: {apartment.email}</p>
-                    <p>Price: ${apartment.price}</p>
-                    <p>Bedrooms: {apartment.bedrooms}</p>
-                    <p>Bathrooms: {apartment.bathrooms}</p>
-                    <p>Pets allowed?: {apartment.pets}</p>
-                    <div className="form-button-wrapper">
-                        {logged_in && 
-                        current_user.id === apartment.user_id &&
-                            <>
-                                <Link to={`/apartmentedit/${apartment.id}`} className="button">
-                                    Edit this Listing
-                                </Link>
-                            </>
-                        }
+                <div className="form-background" style={{ backgroundImage: `url(${image})` }}>
+                    <div className="show-body">
+                        <div className="index-card">
+                            <h3>{apartment.street}</h3>
+                            <p>{apartment.city}, {apartment.state}</p>
+                            <p>Manager Name: {apartment.manager}</p>
+                            <p>Manager Email: {apartment.email}</p>
+                            <p>Price: ${apartment.price}</p>
+                            <p>Bedrooms: {apartment.bedrooms}</p>
+                            <p>Bathrooms: {apartment.bathrooms}</p>
+                            <p>Pets allowed?: {apartment.pets}</p>
+                        </div>
+                        <div className="form-button-wrapper">
+                            {logged_in && 
+                            current_user.id === apartment.user_id &&
+                                <>
+                                    <Link to={`/apartmentedit/${apartment.id}`} className="button">
+                                        Edit this Listing
+                                    </Link>
+                                </>
+                            }
+                        </div>
                     </div>
                 </div>
             </>
