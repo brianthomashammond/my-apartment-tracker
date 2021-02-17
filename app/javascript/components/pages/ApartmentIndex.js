a4a52340e57ace0ff5da440aef43962162e28ba0
brianthomashammond <brianthomashammond@gmail.com>
2021-02-17 15:20:11 -0800

    converts ApartmentIndex to functional component

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,29 +1,28 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import image from '../assets/grant-lemons-jTCLppdwSEc-unsplash.jpg'
 
+const ApartmentIndex = (props) => {
+    const { apartments } = props
 
-export default class ApartmentIndex extends Component {
-    render() {
-        return (
-            <>
-                <div id="index-body" style={{ backgroundImage: `url(${image})` }}>
-                    {this.props.apartments.map((apartment, index) => {
-                        return (
-                            <div key={index} className="index-card">
-                                <h3>{apartment.street}</h3>
-                                <h3>{apartment.city}, {apartment.state}</h3>
-                                <div id="my-buttons">
-                                    <Link to={`/apartmentshow/${apartment.id}`} className="button">
-                                        More Info
-                                    </Link>
-                                </div>
-                            </div>
-                        )
-                    })}
-                </div>
-            </>
-        )
-    }
+    return (
+        <div id="index-body" style={{ backgroundImage: `url(${image})` }}>
+            {apartments.map((apartment, index) => {
+                return (
+                    <div key={index} className="index-card">
+                        <h3>{apartment.street}</h3>
+                        <h3>{apartment.city}, {apartment.state}</h3>
+                        <div id="my-buttons">
+                            <Link to={`/apartmentshow/${apartment.id}`} className="button">
+                                More Info
+                            </Link>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
+
+export default ApartmentIndex
