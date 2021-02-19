d02cecf5315524a3ee8f132e34243b1b88d37f61
brianthomashammond <brianthomashammond@gmail.com>
2021-02-18 18:12:48 -0800

    converts ApartmentShow to functional component

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,38 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import image from '../assets/grant-lemons-jTCLppdwSEc-unsplash.jpg'
 
-export default class ApartmentShow extends Component {
-    render() {
-        const { apartment, current_user, logged_in } = this.props
-        return (
-            <>
-                <div className="form-background" style={{ backgroundImage: `url(${image})` }}>
-                    <div className="show-body">
-                        <div className="index-card">
-                            <h3>{apartment.street}</h3>
-                            <p>{apartment.city}, {apartment.state}</p>
-                            <p>Manager Name: {apartment.manager}</p>
-                            <p>Manager Email: {apartment.email}</p>
-                            <p>Price: ${apartment.price}</p>
-                            <p>Bedrooms: {apartment.bedrooms}</p>
-                            <p>Bathrooms: {apartment.bathrooms}</p>
-                            <p>Pets allowed?: {apartment.pets}</p>
-                        </div>
-                        <div className="form-button-wrapper">
-                            {logged_in && 
-                            current_user.id === apartment.user_id &&
-                                <>
-                                    <Link to={`/apartmentedit/${apartment.id}`} className="button">
-                                        Edit this Listing
-                                    </Link>
-                                </>
-                            }
-                        </div>
-                    </div>
+const ApartmentShow = (props) => {
+    const { apartment, current_user, logged_in } = props
+
+    return (
+        <div className="form-background" style={{ backgroundImage: `url(${image})` }}>
+            <div className="show-body">
+                <div className="index-card">
+                    <h3>{apartment.street}</h3>
+                    <p>{apartment.city}, {apartment.state}</p>
+                    <p>Manager Name: {apartment.manager}</p>
+                    <p>Manager Email: {apartment.email}</p>
+                    <p>Price: ${apartment.price}</p>
+                    <p>Bedrooms: {apartment.bedrooms}</p>
+                    <p>Bathrooms: {apartment.bathrooms}</p>
+                    <p>Pets allowed?: {apartment.pets}</p>
                 </div>
-            </>
-        )
-    }
+                <div className="form-button-wrapper">
+                    {logged_in && 
+                    current_user.id === apartment.user_id &&
+                        <>
+                            <Link to={`/apartmentedit/${apartment.id}`} className="button">
+                                Edit this Listing
+                            </Link>
+                        </>
+                    }
+                </div>
+            </div>
+        </div>
+    )
 }
+
+export default ApartmentShow
