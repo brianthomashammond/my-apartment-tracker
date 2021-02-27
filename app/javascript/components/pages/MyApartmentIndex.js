7137ef04cf3ff0116c412650c0e58036a273dc13
brianthomashammond <brianthomashammond@gmail.com>
2021-02-26 19:44:44 -0800

    converts myApartmentIndex to functional component

--- app/javascript/components/pages/MyApartmentIndex.js
+++ app/javascript/components/pages/MyApartmentIndex.js
@@ -1,38 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 
 import image from '../assets/grant-lemons-jTCLppdwSEc-unsplash.jpg'
 
-export default class MyApartmentIndex extends Component {
-    render() {
-        return (
-            <>
-                <div id="index-body" style={{ backgroundImage: `url(${image})` }}>
-                    {this.props.apartments.map((apartment, index) => {
-                        return (
-                            <div key={index} className="index-card">
-                                <h3>{apartment.street}</h3>
-                                <h3>{apartment.city}, {apartment.state}</h3>
-                                <div id="my-buttons">
-                                    <Link
-                                        to={`/apartmentshow/${apartment.id}`}
-                                        className="button"
-                                    >
-                                        More Info
-                                    </Link>
-                                    <span
-                                        onClick={() => { this.props.deleteApartment(apartment.id) }}
-                                        className="button"
-                                    >
-                                        Remove Listing
-                                    </span>
-                                </div>
-                            </div>
-                        )
-                    })
-                    }
-                </div>
-            </>
-        )
-    }
+const MyApartmentIndex = ({ apartments, deleteApartment }) => {
+    return (
+        <div id="index-body" style={{ backgroundImage: `url(${image})` }}>
+            { apartments.map((apartment, index) => {
+                return (
+                    <div key={index} className="index-card">
+                        <h3>{apartment.street}</h3>
+                        <h3>{apartment.city}, {apartment.state}</h3>
+                        <div id="my-buttons">
+                            <Link
+                                to={`/apartmentshow/${apartment.id}`}
+                                className="button"
+                            >
+                                More Info
+                            </Link>
+                            <span
+                                onClick={() => { deleteApartment(apartment.id) }}
+                                className="button"
+                            >
+                                Remove Listing
+                            </span>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
 }
+
+export default MyApartmentIndex
